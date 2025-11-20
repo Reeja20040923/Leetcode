a147f36274c2c6e350606144295b94d85fb45c11
Yaramalla Reeja <reejayaramalla@gmail.com>
2025-11-20 21:23:14 +0530

    Time: 264 ms (48.21%), Space: 22 MB (72.05%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maximumGap(self, nums):
+        n=len(nums)
+        if n<2:
+            return 0
+        maxi=0
+        nums.sort()
+        for i in range(n-1):
+            maxi=max(maxi,abs(nums[i]-nums[i+1]))        
+        return maxi        
+
+        
